56a30cfca001c3aedda29b8935a4de2fdab9ab9e
Joshua Thijssen <jthijssen@noxlogic.nl>
2016-01-16 23:23:14 +0100

    Initial deployment scheme

---
+++ config/deploy.rb
@@ -0,0 +1,28 @@
+lock '3.4.0'
+
+set :application, 'restcookbook'
+
+set :scm, :git
+set :repo_url, 'https://github.com/restcookbook/restcookbook.git'
+set :branch, "master"
+
+set :format, :pretty
+set :log_level, :debug
+
+set :keep_releases, 5
+
+set :port, 22
+set :deploy_to, '/wwwroot/restcookbook.com'
+set :use_sudo, false
+
+namespace :deploy do
+    task :build_jekyll do
+        on roles(:app) do
+            within "#{deploy_to}/current" do
+                execute :jekyll, "build"
+            end
+        end
+    end
+end
+
+after "deploy:symlink:release", "deploy:build_jekyll"
